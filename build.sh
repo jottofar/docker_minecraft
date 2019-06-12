2a524e29a1e80d1f5c8cf8e0acca596a225d88eb
Jack Ottofaro <28737257+jottofar@users.noreply.github.com>
2019-06-12 13:33:26 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 oc new-project minecraft
-oc new-build https://github.com/Jmainguy/docker_minecraft
+oc new-build https://github.com/jottofar/docker_minecraft
 oc apply -f pvc.yml
 oc apply -f deployment.yaml
 oc apply -f nodePort.yml
